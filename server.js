f7acc91890b47f3bf78ea56aa14f02387f52c745
k2k <kadonna@wonderkul.com>
2019-09-13 21:59:28 -0500

    updated server.js file, aligned with the way the blog activity was set-up

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 5000;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+//var path = require('path');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+var serveStatic = require("serve-static");
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
